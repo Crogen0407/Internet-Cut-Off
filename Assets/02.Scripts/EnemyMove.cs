b5f11b13b7e214bb8d90730dd3643843ad3e0fec
Crogen0407 <mymcoding0407@gmail.com>
2023-12-05 20:08:37 +0900

    feat : EnemyInfo & EnemyType

---
+++ Assets/02.Scripts/EnemyMove.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMove : MonoBehaviour
+{
+    [SerializeField] private float _speed;
+    [SerializeField] private EnemyType _enemyType;
+
+    private void Awake()
+    {
+        
+    }
+
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        
+    }
+}
---
+++ Assets/02.Scripts/SO/SO_EnemyInfoData.cs
@@ -0,0 +1,6 @@
+﻿using UnityEngine;
+
+public class SO_EnemyInfoData : MonoBehaviour
+{
+    
+}
