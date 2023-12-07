e9146b8bf7558d7192a640a8b79e59678c970f81
Crogen0407 <mymcoding0407@gmail.com>
2023-12-07 17:45:38 +0900

    feat : MonoSingleton

--- Assets/02.Scripts/Controllers/StageController.cs
+++ Assets/02.Scripts/Controllers/StageController.cs
@@ -2,10 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class StageController : MonoBehaviour
+public class StageController : MonoSingleton<StageController>
 {
     [SerializeField] private int _currentStage;
     [SerializeField] private List<StageData> stage;
+    
     public int CurrentStage
     {
         get => _currentStage;
@@ -15,6 +16,27 @@ public class StageController : MonoBehaviour
         }
     }
 
+    public List<StageData> Stage
+    {
+        get => stage;
+        set
+        {
+            stage = value;
+            if (stage[_currentStage].yellowEnemyCount <= 0)
+            {
+                NextStage();
+            }
+            else if (stage[_currentStage].blueEnemyCount <= 0)
+            {
+                
+            }
+            else if (stage[_currentStage].redEnemyCount <= 0)
+            {
+                
+            }
+        }
+    }
+    
     /// <summary>
     /// 반환값이 다음 스테이지의 인덱스입니다.
     /// </summary>
